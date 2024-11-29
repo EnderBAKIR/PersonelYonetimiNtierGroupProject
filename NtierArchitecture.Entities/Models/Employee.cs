a0dd0799b06183e8d3d67f9ef1f8fd48a1eb0743
eczmeryem <eczmeryemyilmaz@gmail.com>
2024-11-29 15:54:10 +0300

    abstractions klasörüne BaseEntity ve models klasörüne Employee eklendi

---
+++ NtierArchitecture.Entities/Models/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NtierArchitecture.Entities.Models
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public double Salary { get; set; }
+        public string TcNo { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Adress { get; set; }
+
+    }
+}
